835aa02cc188fda495bbb74436a91985bd1d0364
Venus-Drake <102382422+Venus-Drake@users.noreply.github.com>
2022-12-16 13:52:34 +0200

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -2,9 +2,9 @@
 
 $error = "Could not connect!<br>".__FILE__;
 $my_host = 'localhost';
-$dbuser = 'cristina.ramira';
-$my_pass = 'mapalunara77';
-$mydb = 'warehouse.soft';
+$dbuser = '*********';
+$my_pass = '**********';
+$mydb = '************';
 
 $connect = new mysqli($my_host,$dbuser,$my_pass,$mydb);
 if($connect -> connect_error)
